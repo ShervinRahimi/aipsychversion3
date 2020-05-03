ef3ea001bf2d747fdf5019f2bcc018c1f6b2ac8a
Charlton <thenewtechwiz@gmail.com>
2020-05-03 13:57:17 -0700

    Added database response function and Volley

--- app/src/main/java/com/example/aipsych/SplashActivity.java
+++ app/src/main/java/com/example/aipsych/SplashActivity.java
@@ -1,17 +1,21 @@
 package com.example.aipsych;
 
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.example.aipsych.R;
+
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.time.LocalDate;
 
@@ -48,7 +52,6 @@ public class SplashActivity extends AppCompatActivity implements View.OnClickLis
 
             textView.setText("Name: " + name1 + "\n" + "Email: " + email1 + "\n" + "Gender: " + gender1 + "\n" + "Date of Birth: " + dob1);
 
-;
 
         }
 
@@ -58,7 +61,23 @@ public class SplashActivity extends AppCompatActivity implements View.OnClickLis
 
         myRef.setValue("Hello, World! " + LocalDate.now());
 
-
+        //Write a test message to the dataflow API
+        Controller con = new Controller();
+        try {
+            Volley.newRequestQueue(getApplicationContext()).add(con.getMessage("Hey", new VolleyCallback() {
+                @Override
+                public void onSuccess(String result){
+                    try {
+                        JSONObject json = new JSONObject(result);
+                        Log.d("MESSAGE", json.getJSONObject("result").getJSONObject("fulfillment").getString("speech"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
 }
